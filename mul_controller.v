37daa309e006fe8d08627dc9adf4d834f1d91281
Shivam007018 <iota66445@gmail.com>
2025-04-05 19:19:42 +0530

    Update mul_controller.v

--- mul_controller.v
+++ mul_controller.v
@@ -3,31 +3,29 @@ module CONTROLLER(done, lda, ldb, ldp, decb, clrp, start, clk, eqz); // eqz is u
     output reg done, lda, ldb, ldp, decb, clrp;
     reg [2:0] state;
 
-    // State encoding
+    
     parameter s0 = 3'b000;
     parameter s1 = 3'b001;
     parameter s2 = 3'b010;
     parameter s3 = 3'b011;
     parameter s4 = 3'b100;
 
-    // Initialize state
     initial state = s0;
 
-    // FSM state transitions
     always @(posedge clk) begin
         case(state)
-            s0: if (start) state <= s1;   // if start is 1, move to s1
+            s0: if (start) state <= s1;  
             s1: state <= s2;
             s2: state <= s3;
             s3: if (eqz) state <= s4;
-            s4: state <= s4; // Stay in done state
+            s4: state <= s4; 
             default: state <= s0;
         endcase
     end
 
-    // Output logic
+   
     always @(state) begin
-        // Default values
+       
         lda = 0; ldb = 0; ldp = 0; decb = 0; clrp = 0; done = 0;
 
         case(state)
